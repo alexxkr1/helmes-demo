bb20614682f9b462559038e13eb23468a1234314
Alex Kren <alex.kren@flowit.ee>
2025-10-16 11:46:19 +0300

    Use ResponseEntity for Getting sectors

--- demo-api/src/main/java/com/alex/demo/controller/SectorController.java
+++ demo-api/src/main/java/com/alex/demo/controller/SectorController.java
@@ -3,6 +3,7 @@ package com.alex.demo.controller;
 import com.alex.demo.entity.Sector;
 import com.alex.demo.service.SectorService;
 import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +21,9 @@ public class SectorController {
 
     @Operation(summary = "Get all sectors")
     @GetMapping
-    public List<Sector> getAll() { return service.findAll(); }
+    public ResponseEntity<List<Sector>> getAll() {
+        List<Sector> sectors = service.findAll();
+
+        return ResponseEntity.ok(sectors); 
+    }
 }
