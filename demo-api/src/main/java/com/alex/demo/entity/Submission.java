db8fcda73f6af4934084582ea3d6b579e7586b51
Alex Kren <alex.kren@flowit.ee>
2025-11-09 17:44:26 +0200

    Implement automatic createdAt and updatedAt fields with JPA auditing

--- demo-api/src/main/java/com/alex/demo/entity/Submission.java
+++ demo-api/src/main/java/com/alex/demo/entity/Submission.java
@@ -3,13 +3,18 @@ package com.alex.demo.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.Instant;
 import java.util.List;
 
 @Data
 @Table(name = "submission")
 @Entity
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Submission {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,6 +33,14 @@ public class Submission {
 
     @Column(nullable = false, name = "agreed_to_terms")
     private Boolean agreedToTerms;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
 
     public Submission(Long id, String name, List<Sector> sectors, Boolean agreedToTerms ) {
         this.id = id;
